51c8eaa4cf4160b5e3117f3ce54be37c1873dd48
soon.good <soon.good@outlook.com>
2022-10-14 00:34:30 +0900

    first commit

---
+++ src/main/java/io/study/javadockerjibexample/JavaDockerJibExampleApplication.java
@@ -0,0 +1,13 @@
+package io.study.javadockerjibexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDockerJibExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDockerJibExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/study/javadockerjibexample/JavaDockerJibExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.javadockerjibexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDockerJibExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
